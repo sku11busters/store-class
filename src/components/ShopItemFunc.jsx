e5c47e08751ba39ce5870bae649291850b186531
Nikita Kozyrev <nikk7100@mail.ru>
2024-12-15 14:17:24 +0300

    first commit

---
+++ src/components/ShopItemFunc.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class ShopItemClass extends Component {
+  render() {
+    const { brand, title, description, descriptionFull, price, currency } =
+      this.props.item;
+
+    return (
+      <div className="main-content">
+        <h2>{brand}</h2>
+        <h1>{title}</h1>
+        <h3>{description}</h3>
+        <div className="description">{descriptionFull}</div>
+        <div className="highlight-window mobile">
+          <div className="highlight-overlay"></div>
+        </div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">
+            {currency}
+            {price}
+          </div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ShopItemClass;
